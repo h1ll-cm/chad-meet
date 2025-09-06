bcb9367a0d4b1632448b2112d33814517cc31699
h1ll-cm <scout6964@gmail.com>
2025-09-07 00:36:53 +0200

    Update VideoControls.tsx

--- app/room/[name]/VideoControls.tsx
+++ app/room/[name]/VideoControls.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { Room, Track } from 'livekit-client'
+import { Room, TrackSource } from 'livekit-client';
 
 interface VideoControlsProps {
   room: Room | null

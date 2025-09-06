11484d65862e58589dd0f11b54b9ba3caa0de5e8
h1ll-cm <scout6964@gmail.com>
2025-09-06 18:33:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ const nextConfig = {
   experimental: {
     esmExternals: 'loose',
   },
-  webpack: (config) => {
-    config.externals = config.externals || [];
-    config.externals.push({
-      'livekit-client': 'commonjs livekit-client',
-    });
-    return config;
-  },
 }
 
 module.exports = nextConfig
